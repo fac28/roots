14fc41ed9ce88cb21c2f3c2f93ad40d5ced14249
nichgalzin <nichgalzin@gmail.com>
2023-11-03 15:17:44 +0000

    Chores and initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import { GeistSans } from 'geist/font'
 import './globals.css'
+
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Roots',
+  description: 'Your garden at you fingertips',
 }
 
 export default function RootLayout({
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
+    <html lang="en">
+      <body>
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -21,10 +21,10 @@ export default async function AuthButton() {
   }
 
   return user ? (
-    <div className="flex items-center gap-4">
+    <div>
       Hey, {user.email}!
       <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+        <button>
           Logout
         </button>
       </form>
@@ -32,7 +32,7 @@ export default async function AuthButton() {
   ) : (
     <Link
       href="/login"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+      className="p-2 rounded-md  shadow-lg bg-slate-400"
     >
       Login
     </Link>
