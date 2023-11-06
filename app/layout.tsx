1c4b554dfaaf57ebd961d6f3db12d1ec5aa120f2
tess-phillips <tess.phillips@me.com>
2023-11-06 11:07:55 +0000

    test 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css'
 
-
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : 'http://localhost:3000'
@@ -11,18 +10,15 @@ export const metadata = {
   description: 'Your garden at you fingertips',
 }
 
-
-
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
-        <main className="min-h-screen flex flex-col items-center">
+        <main className='min-h-screen flex flex-col items-center'>
           {children}
         </main>
       </body>
--- components/Header.tsx
+++ components/Header.tsx
@@ -20,11 +20,9 @@ export default function Header() {
 
   return (
     <div>
-        header
-        <nav>
-        <div>
-          {isSupabaseConnected && <AuthButton />}
-        </div>
+      header
+      <nav>
+        <div>{isSupabaseConnected && <AuthButton />}</div>
       </nav>
     </div>
   )
