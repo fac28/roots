afb4aacb54a65cb9a068172485d923b7c921d4a9
tess-phillips <tess.phillips@me.com>
2023-11-06 14:46:01 +0000

    carousel component made and glide installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from '@/components/Header';
+import Carousel from '@/components/Carousel';
 
 export default async function Index() {
   return (
     <>
       <Header />
       <div>Landing Page</div>
+      <Carousel />
     </>
   );
 }
