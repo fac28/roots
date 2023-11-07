ad4d90d1259b6e47d82cdee243df58b3935b42a2
isobelbutler <isobutler@icloud.com>
2023-11-07 15:08:51 +0000

    Can load images on page - fixed query

--- app/unsplash/page.tsx
+++ app/unsplash/page.tsx
@@ -1,22 +1,64 @@
-async function getData() {
-  const accessKey = process.env.UNSPLASH_ACCESS_KEY;
-  const res = await fetch(
-    `https://api.unsplash.com/search/photos?page=1&query=cat&client_id=${accessKey}&per_page=1`
+'use client';
+
+import { useEffect, useState } from 'react';
+
+interface UnsplashImage {
+  id: string;
+  alt_description: string;
+  urls: {
+    full: string;
+    raw: string;
+    regular: string;
+    small: string;
+    thumb: string;
+  };
+}
+
+interface UnsplashApiResponse {
+  total: number;
+  total_pages: number;
+  results: UnsplashImage[];
+}
+
+const Page = () => {
+  const [images, setImages] = useState<UnsplashImage[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const query = 'tomato';
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const accessKey = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY; // Ensure you've set your access key in environment variables
+        const response = await fetch(
+          `https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${accessKey}&per_page=1`
+        );
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        const data: UnsplashApiResponse = await response.json();
+        setImages(data.results);
+      } catch (error) {
+        if (error instanceof Error) setError(error.message);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (error) return <div>Error: {error}</div>;
+
+  return (
+    <main>
+      {images.map((image) => (
+        <div key={image.id}>
+          {/* <h2>{image.alt_description}</h2> */}
+          <img src={image.urls.small} alt={image.alt_description} />
+        </div>
+      ))}
+    </main>
   );
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
+};
 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json();
-}
-
-export default async function Page() {
-  const data = await getData();
-  console.log(data);
-
-  return <main></main>;
-}
+export default Page;
