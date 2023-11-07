307e4197bfdfcf79211759a9db037b8f88ecde94
tess-phillips <tess.phillips@me.com>
2023-11-07 15:58:48 +0000

    jest added

---
+++ jest.config.mjs
@@ -0,0 +1,21 @@
+import nextJest from 'next/jest.js'
+ 
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
+})
+ 
+// Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
+const config = {
+    // Add more setup options before each test is run
+    // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+ 
+    testEnvironment: 'jest-environment-jsdom',
+
+    // Here we tell Jest to look for test files in the __tests__ directory only.
+    testMatch: ['<rootDir>/__tests__/**/*.?(m)js?(x)'], // This will match both .js, .jsx, .mjs files within __tests__ directories
+}
+ 
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+export default createJestConfig(config)
