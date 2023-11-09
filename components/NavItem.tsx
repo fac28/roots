ca07124d47cba9cfaca489e88bd9c91957fc12fe
tess-phillips <tess.phillips@me.com>
2023-11-09 12:44:51 +0000

    nav visibility issue fixed

--- components/NavItem.tsx
+++ components/NavItem.tsx
@@ -1,14 +1,18 @@
 type children = {
   link: string;
   title: string;
+  isHomePage: boolean;
+  toggle: boolean;
 };
 
-const NavItem = ({ link, title }: children) => {
+const NavItem = ({ link, title, isHomePage, toggle }: children) => {
   return (
     <li>
       <a
         href={link}
-        className='md:bg-transparent text-primaryLight hover:text-secondaryGreen hover:transition-colors duration-500 block pl-3 pr-4 py-2 md:p-0 rounded'
+        className={`${
+          isHomePage || !toggle ? 'text-primaryLight' : 'text-primaryDark'
+        } md:bg-transparent hover:text-secondaryGreen hover:transition-colors duration-500 block pl-3 pr-4 py-2 md:p-0 rounded`}
         aria-current='page'
       >
         {title}
