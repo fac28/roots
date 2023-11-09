5965e6bae46e74af40ac8b6d4d214e73aac52be3
tess-phillips <tess.phillips@me.com>
2023-11-09 11:58:08 +0000

    second header removed from mygarden

--- app/(dashboard)/mygarden/layout.tsx
+++ app/(dashboard)/mygarden/layout.tsx
@@ -15,10 +15,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <AuthenticatedHeader />
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
