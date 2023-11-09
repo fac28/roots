576f6be0cfeb10a3253ad2e0f9852605880106c9
isobelbutler <isobutler@icloud.com>
2023-11-09 11:59:43 +0000

    this works but removes duplicate tasks somewhere in line 26-28 filterByUserTasks.ts

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -5,31 +5,39 @@ import { filterByUserTasks } from '@/utils/supabase/models/filterByUserTasks';
 // const tasks = ['Task item one', 'Task item two', 'Task item 3'];
 
 const TaskList = async () => {
-  // !!! userID needs updating to be the actual authenticated user
   const userId = 1;
-  const userTasks = await filterByUserTasks(userId);
-  console.log(userTasks);
-  console.log(JSON.stringify(userTasks, null, 2));
-
-  return (
-    <div className='flex flex-col gap-4 items-center mt-3'>
-      <div className='flex gap-3'>
-        <h2 className=''>Task Summary</h2>
-        <button>
-          <AiOutlinePlus />
-        </button>
-      </div>
-      <ul>
-        {userTasks.map((task, index) => (
-          <li className='flex items-center gap-5 mt-2' key={index}>
-            {/* {task.checked ? <ImCheckboxChecked /> : <ImCheckboxUnchecked />} */}
-            {task.veg_tasks.tasks.task_short}
-          </li>
-        ))}
-      </ul>
-      <div className='bg-slate-300 h-3 w-full'></div>
-    </div>
-  );
+  filterByUserTasks(userId)
+    .then((tasks) => {
+      console.log('Tasks for user', userId, ':', tasks);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
 };
+// !!! userID needs updating to be the actual authenticated user
+// const userId = 1;
+// const userTasks = await filterByUserTasks(userId);
+// console.log(userTasks);
+// console.log(JSON.stringify(userTasks, null, 2));
+
+// return (
+//   <div className='flex flex-col gap-4 items-center mt-3'>
+//     <div className='flex gap-3'>
+//       <h2 className=''>Task Summary</h2>
+//       <button>
+//         <AiOutlinePlus />
+//       </button>
+//     </div>
+//     <ul>
+//       {userTasks.map((task, index) => (
+//         <li className='flex items-center gap-5 mt-2' key={index}>
+//           {/* {task.checked ? <ImCheckboxChecked /> : <ImCheckboxUnchecked />} */}
+//           {task.veg_tasks.tasks.task_short}
+//         </li>
+//       ))}
+//     </ul>
+//     <div className='bg-slate-300 h-3 w-full'></div>
+//   </div>
+// );
 
 export default TaskList;
--- utils/supabase/models/filterByUserTasks.ts
+++ utils/supabase/models/filterByUserTasks.ts
@@ -1,40 +1,80 @@
 import { createClient } from '@/utils/supabase/server';
 import { cookies } from 'next/headers';
 
-type Task = {
-  task: number;
-  veg_tasks: {
-    task_id: number;
-    tasks: {
-      task_short: string;
-    }[];
-  }[];
-};
+const cookieStore = cookies();
+const supabase = createClient(cookieStore);
 
-type UserTasksResults = Task[];
+export async function filterByUserTasks(
+  userId: number
+): Promise<string[] | null> {
+  try {
+    // Execute the SQL query
+    const { data: userTasks, error: userTasksError } = await supabase
+      .from('user_tasks')
+      .select('task')
+      .eq('user_id', userId);
 
-export const filterByUserTasks = async (
-  logged_in_user_id: number
-): Promise<UserTasksResults> => {
-  const cookieStore = cookies();
-  const supabase = createClient(cookieStore);
-  let { data } = await supabase
-    .from('user_tasks')
-    .select(
-      `
-    task,
-    veg_tasks!inner (
-      task_id,
-      tasks!inner (
-        task_short
-      )
-    )
-  `
-    )
-    .eq('user_id', logged_in_user_id);
-  console.log(data);
-  return data || [];
-};
+    if (userTasksError) {
+      throw userTasksError;
+    }
+
+    // Extract task IDs
+    const taskIds: number[] = userTasks.map((row: any) => row.task);
+    console.log(taskIds);
+    // Retrieve task_short values based on task IDs
+    const { data: tasks, error: tasksError } = await supabase
+      .from('tasks')
+      .select('task_short')
+      .in('id', taskIds);
+
+    if (tasksError) {
+      throw tasksError;
+    }
+
+    // Extract and return the task_short values as an array
+    const taskShortList: string[] = tasks.map((row: any) => row.task_short);
+    return taskShortList;
+  } catch (error) {
+    // Handle errors (e.g., log or return an error response)
+    console.error('Error retrieving tasks:', error);
+    throw error;
+  }
+}
+
+// type Task = {
+//   task: number;
+//   veg_tasks: {
+//     task_id: number;
+//     tasks: {
+//       task_short: string;
+//     }[];
+//   }[];
+// };
+
+// type UserTasksResults = Task[];
+
+// export const filterByUserTasks = async (
+//   logged_in_user_id: number
+// ): Promise<UserTasksResults> => {
+//   const cookieStore = cookies();
+//   const supabase = createClient(cookieStore);
+//   let { data } = await supabase
+//     .from('user_tasks')
+//     .select(
+//       `
+//     task,
+//     veg_tasks!inner (
+//       task_id,
+//       tasks!inner (
+//         task_short
+//       )
+//     )
+//   `
+//     )
+//     .eq('user_id', logged_in_user_id);
+//   console.log(data);
+//   return data || [];
+// };
 
 // [
 //   { task: 1, veg_tasks: { task_id: 1, tasks: [Object] } },
