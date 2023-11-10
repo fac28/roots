a67be4ea2107e8fe8018c3be1b42ecd5cb194eb6
tess-phillips <tess.phillips@me.com>
2023-11-10 12:24:08 +0000

    monthly drop down added

--- components/MonthDropdown.tsx
+++ components/MonthDropdown.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { BsChevronCompactDown } from 'react-icons/bs';
+import { BsChevronCompactDown, BsChevronCompactUp } from 'react-icons/bs';
 import { useState } from 'react';
 
 const MonthDropdown = () => {
@@ -8,16 +8,67 @@ const MonthDropdown = () => {
     setToggle((prevState) => !prevState);
   };
 
+  function rotateMonthsToCurrentMonthFirst(months: string[]): string[] {
+    const currentMonthIndex = new Date().getMonth();
+    const rotatedMonths = [
+      ...months.slice(currentMonthIndex),
+      ...months.slice(0, currentMonthIndex),
+    ];
+    return rotatedMonths;
+  }
+
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const rotatedMonths = rotateMonthsToCurrentMonthFirst(months);
+
   return (
-    <>
+    <button className='button gap-2 bg-slate-200 opacity-80 absolute m-2'>
       {toggle ? (
-        <button className='button gap-2 bg-slate-200 opacity-80 absolute m-2'>
-          September <BsChevronCompactDown onClick={handleToggle} />
-        </button>
+        <ul>
+          <li>
+            {rotatedMonths[0]}
+            <BsChevronCompactDown
+              className='inline-block ml-2'
+              onClick={handleToggle}
+            />
+          </li>
+        </ul>
       ) : (
-        <h1>After</h1>
+        <ul>
+          <li>
+            {rotatedMonths[0]}
+            <BsChevronCompactUp
+              className='inline-block ml-2'
+              onClick={handleToggle}
+            />
+          </li>
+          <li>{rotatedMonths[1]}</li>
+          <li>{rotatedMonths[2]}</li>
+          <li>{rotatedMonths[3]}</li>
+          <li>{rotatedMonths[4]}</li>
+          <li>{rotatedMonths[5]}</li>
+          <li>{rotatedMonths[6]}</li>
+          <li>{rotatedMonths[7]}</li>
+          <li>{rotatedMonths[8]}</li>
+          <li>{rotatedMonths[9]}</li>
+          <li>{rotatedMonths[10]}</li>
+          <li>{rotatedMonths[11]}</li>
+          <li>{rotatedMonths[12]}</li>
+        </ul>
       )}
-    </>
+    </button>
   );
 };
 
