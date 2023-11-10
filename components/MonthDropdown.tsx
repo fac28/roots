accc6a649a6d265783321701586aac2d17c5503c
tess-phillips <tess.phillips@me.com>
2023-11-10 11:52:53 +0000

    changing crop down

--- components/MonthDropdown.tsx
+++ components/MonthDropdown.tsx
@@ -1,9 +1,23 @@
+'use client';
 import { BsChevronCompactDown } from 'react-icons/bs';
+import { useState } from 'react';
+
 const MonthDropdown = () => {
+  const [toggle, setToggle] = useState(true);
+  const handleToggle = () => {
+    setToggle((prevState) => !prevState);
+  };
+
   return (
-    <button className='button gap-2 bg-slate-200 opacity-80 absolute m-2'>
-      Setember <BsChevronCompactDown />
-    </button>
+    <>
+      {toggle ? (
+        <button className='button gap-2 bg-slate-200 opacity-80 absolute m-2'>
+          September <BsChevronCompactDown onClick={handleToggle} />
+        </button>
+      ) : (
+        <h1>After</h1>
+      )}
+    </>
   );
 };
 
