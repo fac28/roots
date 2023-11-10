b260009da4b5bba12be2c698da654a4a4bababf3
isobelbutler <isobutler@icloud.com>
2023-11-10 10:45:18 +0000

    Can access user tasks, displays all, has catch for if null, no typescript errors

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -6,13 +6,48 @@ import { filterByUserTasks } from '@/utils/supabase/models/filterByUserTasks';
 
 const TaskList = async () => {
   const userId = 1;
-  filterByUserTasks(userId)
-    .then((tasks) => {
-      console.log('Tasks for user', userId, ':', tasks);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+  const userTasks = await filterByUserTasks(userId);
+
+  if (!userTasks || userTasks.length === 0) {
+    return (
+      <div className='flex flex-col gap-4 items-center mt-3'>
+        <div className='flex gap-3'>
+          <h2 className=''>Task Summary</h2>
+          <button>
+            <AiOutlinePlus />
+          </button>
+        </div>
+        <p>No tasks found for this user.</p>
+        <div className='bg-slate-300 h-3 w-full'></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className='flex flex-col gap-4 items-center mt-3'>
+      <div className='flex gap-3'>
+        <h2 className=''>Task Summary</h2>
+        <button>
+          <AiOutlinePlus />
+        </button>
+      </div>
+      <ul>
+        {userTasks.map((task, index) => (
+          <li className='flex items-center gap-5 mt-2' key={index}>
+            {/* {task.checked ? <ImCheckboxChecked /> : <ImCheckboxUnchecked />} */}
+            {task}
+          </li>
+        ))}
+      </ul>
+      <div className='bg-slate-300 h-3 w-full'></div>
+    </div>
+  );
+  // .then((tasks) => {
+  //   console.log('Tasks for user', userId, ':', tasks);
+  // })
+  // .catch((error) => {
+  //   console.error('Error:', error);
+  // });
 };
 // !!! userID needs updating to be the actual authenticated user
 // const userId = 1;
--- utils/supabase/models/filterByUserTasks.ts
+++ utils/supabase/models/filterByUserTasks.ts
@@ -8,7 +8,7 @@ export async function filterByUserTasks(
   userId: number
 ): Promise<string[] | null> {
   try {
-    // Execute the SQL query
+    // Execute the SQL query to get user tasks
     const { data: userTasks, error: userTasksError } = await supabase
       .from('user_tasks')
       .select('task')
@@ -20,94 +20,27 @@ export async function filterByUserTasks(
 
     // Extract task IDs
     const taskIds: number[] = userTasks.map((row: any) => row.task);
-    console.log(taskIds);
-    // Retrieve task_short values based on task IDs
-    const { data: tasks, error: tasksError } = await supabase
-      .from('tasks')
-      .select('task_short')
-      .in('id', taskIds);
 
-    if (tasksError) {
-      throw tasksError;
+    // Retrieve task_short values one by one for each task ID
+    const taskShortList: string[] = [];
+    for (const taskId of taskIds) {
+      const { data: taskData, error: taskError } = await supabase
+        .from('tasks')
+        .select('task_short')
+        .eq('id', taskId);
+
+      if (taskError) {
+        throw taskError;
+      }
+
+      if (taskData && taskData.length > 0) {
+        taskShortList.push(taskData[0].task_short);
+      }
     }
 
-    // Extract and return the task_short values as an array
-    const taskShortList: string[] = tasks.map((row: any) => row.task_short);
     return taskShortList;
   } catch (error) {
-    // Handle errors (e.g., log or return an error response)
     console.error('Error retrieving tasks:', error);
     throw error;
   }
 }
-
-// type Task = {
-//   task: number;
-//   veg_tasks: {
-//     task_id: number;
-//     tasks: {
-//       task_short: string;
-//     }[];
-//   }[];
-// };
-
-// type UserTasksResults = Task[];
-
-// export const filterByUserTasks = async (
-//   logged_in_user_id: number
-// ): Promise<UserTasksResults> => {
-//   const cookieStore = cookies();
-//   const supabase = createClient(cookieStore);
-//   let { data } = await supabase
-//     .from('user_tasks')
-//     .select(
-//       `
-//     task,
-//     veg_tasks!inner (
-//       task_id,
-//       tasks!inner (
-//         task_short
-//       )
-//     )
-//   `
-//     )
-//     .eq('user_id', logged_in_user_id);
-//   console.log(data);
-//   return data || [];
-// };
-
-// [
-//   { task: 1, veg_tasks: { task_id: 1, tasks: [Object] } },
-//   { task: 3, veg_tasks: { task_id: 3, tasks: [Object] } },
-//   { task: 1, veg_tasks: { task_id: 1, tasks: [Object] } },
-// ];
-
-// [
-//   {
-//     task: 1,
-//     veg_tasks: {
-//       task_id: 1,
-//       tasks: {
-//         task_short: 'Sow Seeds Indoors',
-//       },
-//     },
-//   },
-//   {
-//     task: 3,
-//     veg_tasks: {
-//       task_id: 3,
-//       tasks: {
-//         task_short: 'Install Supports',
-//       },
-//     },
-//   },
-//   {
-//     task: 1,
-//     veg_tasks: {
-//       task_id: 1,
-//       tasks: {
-//         task_short: 'Sow Seeds Indoors',
-//       },
-//     },
-//   },
-// ];
