8b0efbd8544e638860717527417d8dcdbe0982b5
tess-phillips <tess.phillips@me.com>
2023-11-10 10:36:25 +0000

    navList file made so header is <100 now

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
-import NavItem from './Nav/NavItem';
-import LogOutButton from './LogOutButton';
 import NavBurger from './Nav/NavBurger';
+import NavList from './Nav/NavList';
 
 type loggedIn = {
   isLoggedIn: boolean;
@@ -56,52 +55,11 @@ export default function Header({ isLoggedIn }: loggedIn) {
             } notmd md:flex justify-between items-end w-full md:w-auto md:order-1`}
             id='mobile-menu-3'
           >
-            <ul className='items-center flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium pr-4'>
-              <NavItem
-                link='/'
-                title='Home'
-                isHomePage={isHomePage}
-                toggle={toggle}
-              />
-              <NavItem
-                link='/search'
-                title='Find a veggie'
-                isHomePage={isHomePage}
-                toggle={toggle}
-              />
-              <NavItem
-                link='#'
-                title='Take a survey'
-                isHomePage={isHomePage}
-                toggle={toggle}
-              />
-              {isLoggedIn ? (
-                <>
-                  <LogOutButton isHomePage={isHomePage} toggle={toggle} />
-                  <NavItem
-                    link='/mygarden'
-                    title='My garden'
-                    isHomePage={isHomePage}
-                    toggle={toggle}
-                  />
-                </>
-              ) : (
-                <>
-                  <NavItem
-                    link='/signup'
-                    title='Sign up'
-                    isHomePage={isHomePage}
-                    toggle={toggle}
-                  />
-                  <NavItem
-                    link='/login'
-                    title='Log in'
-                    isHomePage={isHomePage}
-                    toggle={toggle}
-                  />
-                </>
-              )}
-            </ul>
+            <NavList
+              toggle={toggle}
+              isHomePage={isHomePage}
+              isLoggedIn={isLoggedIn}
+            />
           </div>
         </div>
       </div>
