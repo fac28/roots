d586fc2f8707fc6b540430d555fc6722fdd88f70
isobelbutler <isobutler@icloud.com>
2023-11-13 15:09:23 +0000

    Moved unplash image api logic to utils

--- app/search/[veg]/page.tsx
+++ app/search/[veg]/page.tsx
@@ -1,54 +1,28 @@
 import { filterByVeggie } from '@/utils/supabase/models/filterByVeggie';
-
+import { fetchUnsplashImage } from '@/utils/supabase/models/fetchUnsplashImage';
 type Params = {
   veg: string;
 };
 
-interface UnsplashImage {
-  id: string;
-  alt_description: string;
-  urls: {
-    full: string;
-    raw: string;
-    regular: string;
-    small: string;
-    thumb: string;
-  };
-}
-interface UnsplashApiResponse {
-  total: number;
-  total_pages: number;
-  results: UnsplashImage[];
-}
-
 const IndividualVegPage = async ({ params }: { params: Params }) => {
   const searchTerm = params.veg;
-
   const veggieResult = await filterByVeggie(searchTerm);
+  const unsplashImage = await fetchUnsplashImage(searchTerm);
 
-  try {
-    const accessKey = process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY;
-    const response = await fetch(
-      `https://api.unsplash.com/search/photos?page=1&query=${searchTerm}&client_id=${accessKey}&per_page=1`
-    );
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const data: UnsplashApiResponse = await response.json();
+  if (unsplashImage) {
     return (
       <>
         <img
-          src={data.results[0].urls.small}
-          alt={data.results[0].alt_description}
+          src={unsplashImage.urls.small}
+          alt={unsplashImage.alt_description}
         />
+
         <h2>Page for {veggieResult[0].name}</h2>
         <p>{veggieResult[0].description}</p>
       </>
     );
-  } catch (error) {
-    console.log('Error fetching data:', error);
+  } else {
+    return <div>Error fetching image.</div>;
   }
 };
 
