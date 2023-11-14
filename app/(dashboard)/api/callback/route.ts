cf0871766f6caa65bdf6dc73f5802000e38e83e8
tess-phillips <tess.phillips@me.com>
2023-11-14 12:29:57 +0000

    cleaning up

--- app/(dashboard)/api/callback/route.ts
+++ app/(dashboard)/api/callback/route.ts
@@ -1,4 +1,3 @@
-// File: app/api/signup.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { fetchIdByVegName } from '@/utils/supabase/models/fetchIdByVegName';
 import { createClient } from '@supabase/supabase-js';
@@ -18,41 +17,39 @@ export default async function handler(
 
     const supabaseUrl: string = process.env.SUPABASE_URL as string; // Ensure the environment variable is treated as a string
     const supabaseKey: string = process.env.SUPABASE_SERVICE_KEY as string; // Same for the service key
-
     const supabase = createClient(supabaseUrl, supabaseKey);
 
-    // //user data
+    //Prepare the users data from supabase
     const name = "User's Name";
     const avatar = "User's Avatar URL";
     const { data: sessionData } = await supabase.auth.getSession();
-
     if (sessionData?.session?.user?.id) {
       const supabase_id = sessionData.session.user.id;
-
-      // Insert the new user row into the 'users' table
-      await supabase
+      // Insert into users
+      const { error: insertError } = await supabase
         .from('users')
         .insert([{ name: name, avatar_url: avatar, supabase_id: supabase_id }]);
-    }
+      if (insertError) {
+        console.error('Error inserting into users:', insertError);
+      }
 
-    // Convert the selected crop names to their respective IDs
-    const vegData = selectedCrops.map((veg) => fetchIdByVegName(supabase, veg));
-
-    // Prepare the user_veg insert data
-    const userVegData = vegData.map((veg) => ({
-      user_id: 6969,
-      veg_id: veg,
-      sown_in: null,
-      sown_dir: null,
-    }));
-
-    // Insert the relations into user_veg table
-    const { error: insertError } = await supabase
-      .from('user_veg')
-      .insert(userVegData);
-
-    if (insertError) {
-      console.error('Error inserting into user_veg:', insertError);
+      // Prepare the user_veg insert data from selectedCrops
+      const vegData = selectedCrops.map((veg) =>
+        fetchIdByVegName(supabase, veg)
+      );
+      const userVegData = vegData.map((veg) => ({
+        user_id: 6969,
+        veg_id: veg,
+        sown_in: null,
+        sown_dir: null,
+      }));
+      // Insert into user_veg
+      const { error: insertError2 } = await supabase
+        .from('user_veg')
+        .insert(userVegData);
+      if (insertError2) {
+        console.error('Error inserting into user_veg:', insertError2);
+      }
     }
 
     res.status(200).json({ message: 'Signup successful' });
