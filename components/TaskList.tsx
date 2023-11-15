116dd3aba9178fda6b9ad8c766c2b9c0cabd1a01
tess-phillips <tess.phillips@me.com>
2023-11-15 13:10:36 +0000

    task list refactor

--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -5,22 +5,6 @@ import getMonthNumber from '@/utils/supabase/models/returnMonthAsNumber';
 
 const TaskList = async ({ searchParams }: any) => {
   const userTasks = await filterByUserTasks();
-
-  if (!userTasks?.taskShortList || userTasks.taskShortList.length === 0) {
-    return (
-      <div className='flex flex-col gap-4 items-center mt-3'>
-        <div className='flex gap-3'>
-          <h2 className=''>Task Summary</h2>
-          <button>
-            <AiOutlinePlus />
-          </button>
-        </div>
-        <p>No tasks found for this user.</p>
-        <div className='bg-slate-300 h-3 w-full'></div>
-      </div>
-    );
-  }
-
   const monthToDisplay = getMonthNumber(searchParams.month);
 
   return (
@@ -31,26 +15,30 @@ const TaskList = async ({ searchParams }: any) => {
           <AiOutlinePlus />
         </button>
       </div>
-      <ul>
-        {userTasks?.taskShortList.map((task, index) => {
-          if (userTasks.taskMonth[index].includes(monthToDisplay)) {
-            return (
-              <li
-                className={
-                  userTasks.taskMonth[index]
-                    .map((number: any) => `_${number}`)
-                    .join(' ') + ' flex items-center gap-5 mt-2'
-                }
-                key={index}
-              >
-                <CustomCheckbox initialState={userTasks.checkedList[index]} />
-                {userTasks.vegNames[index]} - {task}
-              </li>
-            );
-          }
-          return null;
-        })}
-      </ul>
+      {!userTasks?.taskShortList || userTasks.taskShortList.length === 0 ? (
+        <p>No tasks found for this user.</p>
+      ) : (
+        <ul>
+          {userTasks?.taskShortList.map((task, index) => {
+            if (userTasks.taskMonth[index].includes(monthToDisplay)) {
+              return (
+                <li
+                  className={
+                    userTasks.taskMonth[index]
+                      .map((number: any) => `_${number}`)
+                      .join(' ') + ' flex items-center gap-5 mt-2'
+                  }
+                  key={index}
+                >
+                  <CustomCheckbox initialState={userTasks.checkedList[index]} />
+                  {userTasks.vegNames[index]} - {task}
+                </li>
+              );
+            }
+            return null;
+          })}
+        </ul>
+      )}
       <div className='bg-slate-300 h-3 w-full'></div>
     </div>
   );
