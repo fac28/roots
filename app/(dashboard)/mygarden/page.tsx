03cb7b887f014f6dd7ba8dfea5f8a7573e461aff
sgroi-l <laurencesgroi@hotmail.co.uk>
2023-11-15 15:35:34 +0000

    month button

--- app/(dashboard)/mygarden/page.tsx
+++ app/(dashboard)/mygarden/page.tsx
@@ -13,7 +13,7 @@ const MyGarden = ({ searchParams }: { searchParams: { month: string } }) => {
           alt='Autum image'
           width={350}
           height={200}
-          className='overflow-hidden w-full'
+          className='overflow-hidden w-full noimage'
         />
         <TaskList searchParams={searchParams} />
         <WhatsGrowing />
